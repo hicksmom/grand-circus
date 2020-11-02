a76534212e16fe346d68ee211b3742498668b264
Kim Hicks <hicksmomcodes@gmail.com>
2020-11-02 15:39:34 -0500

    Adding html and css for lab 1 part 2

---
+++ exercises/conditions_1/conditions1.js
@@ -0,0 +1,12 @@
+console.log("what up doe?");
+
+let actualTemp = "70";
+let desiredTemp = "72";
+
+if (actualTemp >= 72) {
+    console.log("Run A/C");
+//};
+else if (actualTemp <= 71) {
+    console.log("Run heat");
+}
+
